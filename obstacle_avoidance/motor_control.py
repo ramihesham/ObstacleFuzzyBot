9563cf8f02b65cd6df8f7050714c31ef70991d5a
Ahmed Magdi <128742811+Ahmed-Magdi1@users.noreply.github.com>
2023-10-27 14:53:21 +0200

    Pins and rules modifications

--- obstacle_avoidance/motor_control.py
+++ obstacle_avoidance/motor_control.py
@@ -1,87 +1,93 @@
-from machine import Pin, PWM
+from machine import Pin,PWM
 import time 
-import utime
-import fuzzy_w_ultrasonic
+#import fuzzy_w_ultrasonic
 
-Lfwd_motor = Pin(5,Pin.OUT)
-Rfwd_motor = Pin(6,Pin.OUT)
-Lrev_motor = Pin(7,Pin.OUT)
-Rrev_motor = Pin(8,Pin.OUT)
+ENA = PWM(Pin(4)) #The main control on IN1 and IN2
+IN1 = Pin(5,Pin.OUT)
+IN2 = Pin(6,Pin.OUT)
+IN3 = Pin(7,Pin.OUT)
+IN4 = Pin(8,Pin.OUT)
+ENB = PWM(Pin(9)) #The main control on IN3 and IN4
 
-enable1 = PWM(Pin(12)) #allows you to give analogue behaviours to digital devices
-enable2 = PWM(Pin(13))
+ENA.freq(1000) #PWM interval periods per second
+ENB.freq(1000)
 
-enable1.freq(1000) #PWM interval periods per second
-enable2.freq(1000)
-
-enable1.duty_u16(20000) #motor speed
-enable2.duty_u16(20000) 
+speed = 30000 #the speed of the robot
+ 
+#enable1.duty_u16(20000) #motor speed
+#enable2.duty_u16(20000) 
 
 # Define IR sensor pins
-left_sensor = Pin(9, Pin.IN)
-right_sensor = Pin(10, Pin.IN)
+#left_sensor = Pin(9, Pin.IN)
+#right_sensor = Pin(10, Pin.IN)
 
 def forward():
-
-    Lfwd_motor.high()
-    Rfwd_motor.high()
-    Lrev_motor.low() 
-    Rrev_motor.low()
+    ENA.duty_u16(speed)
+    IN1.value(0)
+    IN1.value(1)
+    ENB.duty_u16(speed)
+    IN1.value(1) 
+    IN1.value(0)
     
 def backward():
-
-    Lfwd_motor.low()
-    Rfwd_motor.low()
-    Lrev_motor.high() 
-    Rrev_motor.high()
+    ENA.duty_u16(speed)
+    IN1.value(1)
+    IN1.value(0)
+    ENB.duty_u16(speed)
+    IN1.value(0) 
+    IN1.value(1)
 
 def right():
-
-    Rfwd_motor.low()
-    Lfwd_motor.high()
-    Lrev_motor.high() 
-    Rrev_motor.low()
+    ENA.duty_u16(speed)
+    IN1.value(0)
+    IN1.value(1)
+    ENB.duty_u16(speed)
+    IN1.value(0) 
+    IN1.value(1)
     
 def left():
-    Rfwd_motor.high()
-    Lfwd_motor.low()
-    Lrev_motor.high() 
-    Rrev_motor.low()
+    ENA.duty_u16(speed)
+    IN1.value(1)
+    IN1.value(0)
+    ENB.duty_u16(speed)
+    IN1.value(1) 
+    IN1.value(0)
    
 def stop():
-
-    Lfwd_motor.low()
-    Rfwd_motor.low()
-    Lrev_motor.low()
-    Rrev_motor.low()
+    ENA.duty_u16(0)
+    IN1.value(0)
+    IN1.value(0)
+    ENB.duty_u16(0)
+    IN1.value(0) 
+    IN1.value(0)
 
 
     #if the avoidance decision has a output from the ultrasonic
-while True:
+#while True:
 
         #obsticale avoider
-    if 0 <= fuzzy_w_ultrasonic.avoidance_decision.output < 34:
-        left()
-    elif 34 <= fuzzy_w_ultrasonic.avoidance_decision.output <= 66:
-        forward()
-    elif 67 <= fuzzy_w_ultrasonic.avoidance_decision.output <= 100:
-        right()
-    else:
-        stop()
+#    if 0 <= fuzzy_w_ultrasonic.avoidance_decision.output < 34:
+ #       left()
+  #  elif 34 <= fuzzy_w_ultrasonic.avoidance_decision.output <= 66:
+   #     forward()
+    #elif 67 <= fuzzy_w_ultrasonic.avoidance_decision.output <= 100:
+     #   right()
+    #else:
+     #   stop()
         
-    time.sleep(0.1) 
+    #time.sleep(0.1) 
 
-else: 
+#else: 
         #line follower
 
-    left_detected = left_sensor.value() #read IR
-    right_detected = right_sensor.value()   
+ #   left_detected = left_sensor.value() #read IR
+  #  right_detected = right_sensor.value()   
         
-    if left_detected and not right_detected:   
-        left()
-    elif not left_detected and right_detected:
-        right()
-    elif left_detected and right_detected:
-        forward()
-    else:
-        stop()  
+   # if left_detected and not right_detected:   
+    #    left()
+    #elif not left_detected and right_detected:
+     #   right()
+    #elif left_detected and right_detected:
+     #   forward()
+    #else:
+     #   stop()  
