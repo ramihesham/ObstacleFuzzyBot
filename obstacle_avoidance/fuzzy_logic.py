d83509c49e7b78b455a80436cff89ae13b087753
ramihesham <148961249+ramihesham@users.noreply.github.com>
2023-11-05 23:20:21 +0200

    Update fuzzy_logic.py

--- obstacle_avoidance/fuzzy_logic.py
+++ obstacle_avoidance/fuzzy_logic.py
@@ -6,18 +6,38 @@ import utime
 from obstacle_avoidance import ultrasonic as us
 
 # Define linguistic variables
-distance = ctrl.Antecedent(np.arange(0, 101, 1), 'distance')
-action = ctrl.Consequent(np.arange(0, 101, 1), 'action')
+right_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'right_sensor_reading')
+left_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'left_sensor_reading')
+front_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'front_sensor_reading')
+left_motor = ctrl.Consequent(np.arange(-200, 200, 50), 'left_motor_RPM')
+right_motor = ctrl.Consequent(np.arange(-200, 200, 50), 'right_motor_RPM')
 
-# Membership functions for Distance
-distance['near'] = fuzz.trimf(distance.universe, [0, 0, 25])
-distance['medium'] = fuzz.trimf(distance.universe, [0, 50, 100])
-distance['far'] = fuzz.trimf(distance.universe, [75, 100, 100])
+# Membership functions for right_sensor
+right_sensor['near'] = fuzz.trapmf(right_sensor_reading.universe, [2, 2, 50, 150])
+right_sensor['medium'] = fuzz.trimf(right_sensor_reading.universe, [100, 200, 300])
+right_sensor['far'] = fuzz.trapmf(right_sensor_reading.universe, [250, 350, 400, 400])
 
-# Membership functions for Action
-action['turn_left'] = fuzz.trimf(action.universe, [0, 0, 50])
-action['go_straight'] = fuzz.trimf(action.universe, [25, 50, 75])
-action['turn_right'] = fuzz.trimf(action.universe, [50, 100, 100])
+# Membership functions for left_sensor
+left_sensor['near'] = fuzz.trapmf(left_sensor_reading.universe, [2, 2, 50, 150])
+left_sensor['medium'] = fuzz.trimf(left_sensor_reading.universe, [100, 200, 300])
+left_sensor['far'] = fuzz.trapmf(left_sensor_reading.universe, [250, 350, 400, 400])
+
+# Membership functions for front_sensor
+front_sensor['near'] = fuzz.trapmf(front_sensor_reading.universe, [2, 2, 50, 150])
+front_sensor['medium'] = fuzz.trimf(front_sensor_reading.universe, [100, 200, 300])
+front_sensor['far'] = fuzz.trapmf(front_sensor_reading.universe, [250, 350, 400, 400])
+
+# Membership functions for left_motor
+left_motor['turn_left'] = fuzz.trapmf(left_motor_RPM.universe, [-200, -200, 45, 90])
+left_motor['go_straight'] = fuzz.trimf(left_motor_RPM.universe, [-90, 0, 90])
+left_motor['turn_right'] = fuzz.trapmf(left_motor_RPM.universe, [76.4, 76.4, 104, 159])
+left_motor['turn_right'] = fuzz.trapmf(left_motor_RPM.universe, [117.5, 172.5, 200, 200])
+
+# Membership functions for right_motor
+right_motor['turn_left'] = fuzz.trapmf(right_motor_RPM.universe, [-200, -200, 45, 90])
+right_motor['go_straight'] = fuzz.trimf(right_motor_RPM.universe, [-90, 0, 90])
+right_motor['turn_right'] = fuzz.trapmf(right_motor_RPM.universe, [76.4, 76.4, 104, 159])
+right_motor['turn_right'] = fuzz.trapmf(right_motor_RPM.universe, [117.5, 172.5, 200, 200])
 
 # Fuzzy rules
 rule1 = ctrl.Rule(distance['near'], action['turn_left'])
