226c833377ae7e29f690db2638d936e9477bdd39
ramihesham <148961249+ramihesham@users.noreply.github.com>
2023-11-12 00:35:27 +0200

    Update fuzzy_logic.py

--- obstacle_avoidance/fuzzy_logic.py
+++ obstacle_avoidance/fuzzy_logic.py
@@ -6,26 +6,26 @@ import utime
 from obstacle_avoidance import ultrasonic as us
 
 # Define linguistic variables
-right_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'right_sensor_reading')
-left_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'left_sensor_reading')
-front_sensor = ctrl.Antecedent(np.arange(2, 400, 50), 'front_sensor_reading')
+left_ultrasonic = ctrl.Antecedent(np.arange(0, 400, 50), 'left_ultrasonic_reading')
+front_ultrasonic = ctrl.Antecedent(np.arange(0, 400, 50), 'front_ultrasonic_reading')
+right_ultrasonic = ctrl.Antecedent(np.arange(0, 400, 50), 'right_ultrasonic_reading')
 left_motor = ctrl.Consequent(np.arange(-200, 200, 50), 'left_motor_RPM')
 right_motor = ctrl.Consequent(np.arange(-200, 200, 50), 'right_motor_RPM')
 
-# Membership functions for right_sensor
-right_sensor['near'] = fuzz.trapmf(right_sensor_reading.universe, [2, 2, 50, 150])
-right_sensor['medium'] = fuzz.trimf(right_sensor_reading.universe, [100, 200, 300])
-right_sensor['far'] = fuzz.trapmf(right_sensor_reading.universe, [250, 350, 400, 400])
+# Membership functions for left_ultrasonic
+left_ultrasonic['near'] = fuzz.trapmf(left_ultrasonic_reading.universe, [2, 2, 50, 150])
+left_ultrasonic['medium'] = fuzz.trimf(left_ultrasonic_reading.universe, [100, 200, 300])
+left_ultrasonic['far'] = fuzz.trapmf(left_ultrasonic_reading.universe, [250, 350, 400, 400])
 
-# Membership functions for left_sensor
-left_sensor['near'] = fuzz.trapmf(left_sensor_reading.universe, [2, 2, 50, 150])
-left_sensor['medium'] = fuzz.trimf(left_sensor_reading.universe, [100, 200, 300])
-left_sensor['far'] = fuzz.trapmf(left_sensor_reading.universe, [250, 350, 400, 400])
+# Membership functions for front_ultrasonic
+front_ultrasonic['near'] = fuzz.trapmf(front_ultrasonic_reading.universe, [2, 2, 50, 150])
+front_ultrasonic['medium'] = fuzz.trimf(front_ultrasonic_reading.universe, [100, 200, 300])
+front_ultrasonic['far'] = fuzz.trapmf(front_ultrasonic_reading.universe, [250, 350, 400, 400])
 
-# Membership functions for front_sensor
-front_sensor['near'] = fuzz.trapmf(front_sensor_reading.universe, [2, 2, 50, 150])
-front_sensor['medium'] = fuzz.trimf(front_sensor_reading.universe, [100, 200, 300])
-front_sensor['far'] = fuzz.trapmf(front_sensor_reading.universe, [250, 350, 400, 400])
+# Membership functions for right_ultrasonic
+right_ultrasonic['near'] = fuzz.trapmf(right_ultrasonic_reading.universe, [2, 2, 50, 150])
+right_ultrasonic['medium'] = fuzz.trimf(right_ultrasonic_reading.universe, [100, 200, 300])
+right_ultrasonic['far'] = fuzz.trapmf(right_ultrasonic_reading.universe, [250, 350, 400, 400])
 
 # Membership functions for left_motor
 left_motor['-ve_fast'] = fuzz.trapmf(left_motor_RPM.universe, [-200, -200, 45, 90])
@@ -40,12 +40,36 @@ right_motor['+ve_slow'] = fuzz.trapmf(right_motor_RPM.universe, [76.4, 76.4, 104
 right_motor['+ve_fast'] = fuzz.trapmf(right_motor_RPM.universe, [117.5, 172.5, 200, 200])
 
 # Fuzzy rules
-rule1 = ctrl.Rule(distance['near'], action['turn_left'])
-rule2 = ctrl.Rule(distance['medium'], action['go_straight'])
-rule3 = ctrl.Rule(distance['far'], action['turn_right'])
+rule1 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['near'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule2 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['near'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule3 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['near'] & right_ultrasonic['far'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule4 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['medium'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule5 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['medium'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule6 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['medium'] & right_ultrasonic['far'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule7 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['far'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule8 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['far'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule9 = ctrl.Rule(left_ultrasonic['near'] & front_ultrasonic['far'] & right_ultrasonic['far'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule10 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['near'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule11 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['near'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule12 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['near'] & right_ultrasonic['far'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule13 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['medium'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule14 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['medium'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['+ve_slow'])
+rule15 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['medium'] & right_ultrasonic['far'], left_motor['+ve_fast'], right_motor['+ve_slow'])
+rule16 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['far'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule17 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['far'] & right_ultrasonic['medium'], left_motor['+ve_fast'], right_motor['+ve_slow'])
+rule18 = ctrl.Rule(front_ultrasonic['medium'] & front_ultrasonic['far'] & right_ultrasonic['far'], left_motor['+ve_fast'], right_motor['+ve_slow'])
+rule19 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['near'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule20 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['near'] & right_ultrasonic['medium'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule21 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['near'] & right_ultrasonic['far'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule22 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['medium'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule23 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['medium'] & right_ultrasonic['medium'], left_motor['+ve_slow'], right_motor['+ve_fast'])
+rule24 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['medium'] & right_ultrasonic['far'], left_motor['+ve_fast'], right_motor['+ve_slow'])
+rule25 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['far'] & right_ultrasonic['near'], left_motor['-ve_fast'], right_motor['-ve_fast'])
+rule26 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['far'] & right_ultrasonic['medium'], left_motor['+ve_slow'], right_motor['+ve_fast'])
+rule27 = ctrl.Rule(left_ultrasonic['far'] & front_ultrasonic['far'] & right_ultrasonic['far'], left_motor['+ve_fast'], right_motor['+ve_fast'])
 
 # Control system
-obstacle_avoidance = ctrl.ControlSystem([rule1, rule2, rule3])
+obstacle_avoidance = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, rule19, rule20, rule21, rule22, rule23, rule24, rule25, rule26, rule27])
 avoidance_decision = ctrl.ControlSystemSimulation(obstacle_avoidance)
 
 # Test
